227c0756662836a9512eb4827c465632df5937e9
1194239101 <1194239101@qq.com>
2018-05-17 08:20:29 +0800

    Initial commit

---
+++ app/src/main/gen/com/example/a94868/mywordsapp/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.a94868.mywordsapp;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
